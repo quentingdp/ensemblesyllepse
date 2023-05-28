48fec3b413f488823f12a366e52ef3570f27a69c
quentingdp <q.guillon@mailo.com>
2023-05-28 19:23:12 +0200

    tech: modification d'un fichier pour vérification migration

--- front/src/components/Navigation.test.js
+++ front/src/components/Navigation.test.js
@@ -1,7 +1,7 @@
 import Navigation from "./Navigation"
 import { render } from "@testing-library/react"
 
-describe("Le composant de Navigation", () => {
+describe("Le composant Navigation", () => {
     test("doit s'afficher sans erreur", async () => {
         render(<Navigation />)
     })
